0a2fa274318702a2ab912b0cf826e91ff0b61aeb
khai <pvkk224@gmail.com>
2022-11-22 12:56:15 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Routes,Route, BrowserRouter as Router} from 'react-router-dom';
+import RootRouter  from './router/RootRouter';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <div>
+          <div>
+          {/* <MenuBar/> */}
+          <Routes>
+    {showMenuContents(RootRouter,locationContext)}
+    </Routes>
+              
+          </div>
+      </div>
+        {/* <MyFooter/> */}
+    </Router>
+    )
+}
+
+const showMenuContents=(routers,locationContext)=>{
+  var index=0;
+  var routerList=[]
+  routers.forEach(element => {
+    var rout= element.map((route)=>{
+      return(
+        <Route key={++index} path={route.path} exact={route.exact} element={<route.page/>}  />
+      )
+      })
+      console.log(rout);
+      routerList.push(...rout);
+  }
   );
+  console.log(routerList)
+  return routerList;
 }
 
 export default App;
---
+++ src/router/RootRouter.js
@@ -0,0 +1,1 @@
+export default RootRouter = []
